5a5b25c9b3ee2b730752ddd4a8546b9ec8febb6f
Lectem <lectem@gmail.com>
2020-12-24 00:32:33 +0100

    Add USER_ERROR macros for user facing errors

--- source/include/Log.h
+++ source/include/Log.h
@@ -3,3 +3,6 @@
 #include <fmt/format.h>
 #define LOG(fmtstr, ...) OutputDebugStringA(fmt::format(LOG_PREFIX fmtstr, __VA_ARGS__).c_str())
 #define LOGW(fmtstr, ...) OutputDebugStringW(fmt::format(LOG_PREFIX fmtstr, __VA_ARGS__).c_str())
+
+#define USER_ERROR(fmtstr, ...) MessageBoxA(nullptr, fmt::format(LOG_PREFIX fmtstr, __VA_ARGS__).c_str(), nullptr, MB_OK)
+#define USER_ERRORW(fmtstr, ...) MessageBoxW(nullptr, fmt::format(LOG_PREFIX fmtstr, __VA_ARGS__).c_str(), nullptr, MB_OK)
